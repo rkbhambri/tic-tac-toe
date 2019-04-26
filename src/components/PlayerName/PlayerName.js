c0077446f41bff8db3a0fbe9e8daec93e8da904b
ram <ramkrishan@kontikilabs.com>
2019-04-26 17:36:55 +0530

    Minor change to show winner and made changes for deployment

--- src/components/PlayerName/PlayerName.js
+++ src/components/PlayerName/PlayerName.js
@@ -1,35 +1,35 @@
 import React from 'react';
 
 const PlayerName = (props) => {
-    return (
-        <form
-            className="player row col-md-8 offset-2 mt-4"
-            onChange={(event) => props.nameChangeHandler(event)}>
-            <input
-                type="text"
-                className="form-control col-md-4"
-                name="player1"
-                defaultValue={props.player.player1.name}
-                placeholder="Enter name"
-                readOnly={props.isPlayerAdded} />
-            {
-                props.gameType === 'double' &&
-                <input
-                    type="text"
-                    className="form-control col-md-4 ml-2"
-                    name="player2"
-                    defaultValue={props.player.player2.name}
-                    placeholder="Enter name"
-                    readOnly={props.isPlayerAdded} />
-            }
-            <button
-                className="btn btn-sm btn-primary col-md-3 ml-3"
-                type="button"
-                onClick={props.addPlayer}>
-                Add
+	return (
+		<form
+			className="player row col-md-8 offset-2 mt-4"
+			onChange={(event) => props.nameChangeHandler(event)}>
+			<input
+				type="text"
+				className="form-control col-md-4"
+				name="player1"
+				defaultValue={props.player.player1.name}
+				placeholder="Enter name"
+				readOnly={props.isPlayerAdded} />
+			{
+				props.gameType === 'double' &&
+				<input
+					type="text"
+					className="form-control col-md-4 ml-2"
+					name="player2"
+					defaultValue={props.player.player2.name}
+					placeholder="Enter name"
+					readOnly={props.isPlayerAdded} />
+			}
+			<button
+				className="btn btn-sm btn-primary col-md-3 ml-3"
+				type="button"
+				onClick={props.addPlayer}>
+				Add
         </button>
-        </form>
-    );
+		</form>
+	);
 };
 
 export default PlayerName;
