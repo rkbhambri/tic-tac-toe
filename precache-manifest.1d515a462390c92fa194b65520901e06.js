87bb0b893148e7575dbf75572ad566fcb241d241
ram <ramkrishan@kontikilabs.com>
2019-04-26 17:38:56 +0530

    Updates

---
+++ precache-manifest.1d515a462390c92fa194b65520901e06.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "9728fa41a930f12fdc87",
+    "url": "/tic-tac-toe/static/js/runtime~main.f1e29306.js"
+  },
+  {
+    "revision": "aa43304ffec3641da776",
+    "url": "/tic-tac-toe/static/js/main.54ef0135.chunk.js"
+  },
+  {
+    "revision": "1048c6d6b1ae7b3cbe2c",
+    "url": "/tic-tac-toe/static/js/2.dbbc0f9e.chunk.js"
+  },
+  {
+    "revision": "aa43304ffec3641da776",
+    "url": "/tic-tac-toe/static/css/main.bc67ffc5.chunk.css"
+  },
+  {
+    "revision": "1048c6d6b1ae7b3cbe2c",
+    "url": "/tic-tac-toe/static/css/2.9446f2d5.chunk.css"
+  },
+  {
+    "revision": "d311e01937541f5db33d58677a2fc54b",
+    "url": "/tic-tac-toe/index.html"
+  }
+];
